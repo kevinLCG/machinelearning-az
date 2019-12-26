7a17231c11c58196e1cd49aae6f99a099ce50550
Kevin Meza <kevinml@lcg.unam.mx>
2019-12-26 09:41:53 -0600

    documentation

--- datasets/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
+++ datasets/Part 2 - Regression/Section 5 - Multiple Linear Regression/multiple_linear_regression.R
@@ -1,6 +1,6 @@
 # =================================================================================================
 # NOTAS IMPORTANTES:
-# 
+#
 # Para que podamos aplicar la regresion lineal multiple, hay que tener en cuenta que nuestro modelo
 # tiene que cumplir con lo siguiente:
 # 1.- Linealidad
@@ -9,7 +9,7 @@
 # 3.- Normalidad Multivariable
 # 4.- Independencia de los Errores
 # 5.- Ausencia de Multicolinealidad
-# 
+#
 # Si hay variables categoricas, se tienen que convertir a OneHot Encoders (variables Dummy).
 # El (numero de categorias-1) sera la cantidad de parametros que tendra el modelo de regresion lineal.
 # Ej: categorias: rojo, azul. Entonces: y = b0 + ... + bn*D1 (no ponemos bn+1*D2)
@@ -18,12 +18,12 @@
 #
 # =================================================================================================
 # CREACION DEL MODELO DE REGRESION LINEAL MULTIPLE:
-# 
+#
 # 1.- Exahustivo (All-in):	Incluir todas las variables. Util como preparacion previa para luego implementar el 2.
 # 2.- Eliminacion hacia atras:
 #                               a) Elegir nivel de significación (S.L.) para que una variable permanezca en el modelo; normalmente se usa S.L.= 0.05.
 #                               b) Se calcula el modelo con todas las variables predictoras (all-in)
-#                               c) Se elige la variable predictora con el -p-value mas grande. Si su p-value es mayor que S.L. se elimina dicha variable.
+#                               c) Se elige la variable predictora con el p-value mas grande. Si su p-value es mayor que S.L. se elimina dicha variable.
 #                               d) Ajustar el modelo sindicha variable.
 #                               e) Repetir pasos c) y d) hasta que la variable con el p-value mas grande sea menor al S.L.
 # 3.- Seleccion hacia adelante:
@@ -31,26 +31,25 @@
 #                               b) Ajustamos todos los modelos de regresion lineal simple; utilizando cada una de las variables independientes con respecto a la dependiente (y~Xn).
 #                                  Se elige el modelo con el p-value mas pequeño y conservamos dicha variable
 #                               c) Ajustamos todos los posibles modelos con una variable extra a las que ya tenga el modelo hasta ese momento.
-#                               d) Consideramos la variable predictora con el p-value mas pequeño. Si su p-value es menor que S.L., repetimos c), sino FIN y nos quedamos con el modelo anterior. 
-# 4.- Eliminacion Bidireccional: 
+#                               d) Consideramos la variable predictora con el p-value mas pequeño. Si su p-value es menor que S.L., repetimos c), sino FIN y nos quedamos con el modelo anterior.
+# 4.- Eliminacion Bidireccional:
 #                               a) Elegir nivel de significación (S.L.) para que una variable entre en el modelo (SLENTER) y otra para que permanezca (SLSTAY); normalmente se usa S.L.= 0.05.
 #                               b) Realozamos el paso b) de "Seleccion hacia adelante" y nos quedamos con un modelo.
 #                               c) Realizamos TODOS los pasos de "Eliminacion hacia atras". Las variables antiguas deben tener p-value < SLSTAY (pudiendo eliminar mas de una).
 #                               d) Repetir b) y c) hasta que no haya variables ni para entrar ni para salir.
-# 5.- Compacarion de scores: Generar TODOS los modelos posibles, todos los de 1 variable, todos los de 2 variables, etc. 
-#                            En total habra (2^n)-1 modelos; donde n es el numero de variables.
-#                            NOTA: No es el mejor modelo cuando se tienen muchas variables.
-#                            a) Elegir un criterio de bondad del ajuste (p.e. criterio bayesiano basandonos en R^2, Akaike)
-#                            b) Selecionar el modelo con le mejor scroe de acuerdo al criterio elejido.
+# 5.- Compacarion de scores
 #
 # NOTA: 2,3 y 4 forman parte de la familia "Regresion Paso a Paso"
 #
+# Si un coeficiente de alguna variable, dentro de la fórmula de regresion lineal multiple es cero o cercano a cero,
+# la variable no deberia de ser considerada.
+# El p-valueu nos da una idea sobre que tan probable es que un coeficiente de la RLM sea igual a cero.
 # =================================================================================================
 
-# Regresión Lineal Múltiple
+# REGRESIÓN LINEAL MÚLTIPLE
 
 # Importar el dataset
-dataset = read.csv('50_Startups.csv')
+dataset = read.csv('/home/kevinml/Documentos/Udemy/machinelearning-az/datasets/Part 2 - Regression/Section 5 - Multiple Linear Regression/50_Startups.csv')
 #dataset = dataset[, 2:3]
 
 # Codificar las variables categóricas
@@ -58,10 +57,14 @@ dataset$State = factor(dataset$State,
                          levels = c("New York", "California", "Florida"),
                          labels = c(1, 2, 3))
 
-
+# ===========================================================================
+# ¡OJO!
+# Aqui no se tuvo la necesidad de evitar la "trampa de las variables Dummy",
+# esto porque la libreria catools ya lo hace al convertir las variables categoricas
+# a factores, evitando asi el problema de multicolinealidad.
+# ===========================================================================
 
 # Dividir los datos en conjunto de entrenamiento y conjunto de test
-# install.packages("caTools")
 library(caTools)
 set.seed(123)
 split = sample.split(dataset$Profit, SplitRatio = 0.8)
@@ -73,18 +76,23 @@ testing_set = subset(dataset, split == FALSE)
 # testing_set[,2:3] = scale(testing_set[,2:3])
 
 # Ajustar el modelo de Regresión Lineal Múltiple con el Conjunto de Entrenamiento
+# El punto hace refrencia a todas las demas variables, es lo mismo que: R.D.Spend + Administration + Marketing.Spend + State
 regression = lm(formula = Profit ~ .,
                 data = training_set)
+summary(regression)
 
 # Predecir los resultados con el conjunto de testing
 y_pred = predict(regression, newdata = testing_set)
 
-# Construir un modelo óptimo con la Eliminación hacia atrás
+# Construir un modelo óptimo con la Eliminación hacia atrás.
+# Empezamos  utilizando un modelo que considera todas las variables.
 SL = 0.05
 regression = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend + State,
                 data = dataset)
 summary(regression)
 
+# Eliminamos la columna State, fijarse que se quitan todas las variables dummy,
+# pues se esta eliminando la columna completa.
 regression = lm(formula = Profit ~ R.D.Spend + Administration + Marketing.Spend,
                 data = dataset)
 summary(regression)
@@ -96,3 +104,26 @@ summary(regression)
 regression = lm(formula = Profit ~ R.D.Spend,
                 data = dataset)
 summary(regression)
+
+
+############################################################################
+# ELIMINACION HACIA ATRAS AUTOMATICA
+backwardElimination <- function(x, sl) {
+numVars = length(x)
+for (i in c(1:numVars)){
+  regressor = lm(formula = Profit ~ ., data = x)
+  maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+  if (maxVar > sl){
+    j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+    x = x[, -j]
+  }
+  numVars = numVars - 1
+}
+return(summary(regressor))
+}
+
+SL = 0.05
+dataset = dataset[, c(1,2,3,4,5)]
+backwardElimination(training_set, SL)
+ 
+############################################################################
--- datasets/Part 2 - Regression/Section 6 - Polynomial Regression/polinomial_regression.py
+++ datasets/Part 2 - Regression/Section 6 - Polynomial Regression/polinomial_regression.py
@@ -3,70 +3,146 @@
 """
 Created on Tue Mar  5 12:45:44 2019
 
-@author: juangabriel
+@author: juangabriel and Kevin Meza
 """
 
-# Regresión polinómica
+####################################################################################################
+#
+# En una regresion poinomial se tiene una variable, pero esta esta elevada a deferentes potencias.
+# y = b + b1x1 + b2x1^1 + b3x1^2 + b4x1^3 + ... + bnx1^n
+# Este tipo de regresion es util cuando al graficar los datos, una curva los podria describir de mejor 
+# manera que una recta, como en el caso de una exponencial.
+#
+####################################################################################################
+
+
+# REGRESIÓN POLINOMIAL
 
 # Cómo importar las librerías
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importar el data set
+################################################
+###          IMPORTAR EL DATA SET            ###
+################################################
+
 dataset = pd.read_csv('Position_Salaries.csv')
+# En este caso no timaremos en cuenta la primer columna, pues provee la 
+# misma informacion que la segunda columna.
+
+# X = dataset.iloc[:, 1].values
+# Para los algoritmos de Machine Learning, se necesita una MATRIZ de caracteristicas.
+# La sintaxis de arriba nos arroja un vector, mientras que al filtrar la informacion
+# poniendo "1:2" en vez de "1", obtenemos como resultado una matriz.
 X = dataset.iloc[:, 1:2].values
 y = dataset.iloc[:, 2].values
 
+#################################################################################
+### Dividir el data set en conjunto de entrenamiento y conjunto de testing    ###
+#################################################################################
 
-# Dividir el data set en conjunto de entrenamiento y conjunto de testing
+# En este caso no vamos a dvidir nuestro conjunto de datos, ya que contamos con 
+# un escaso numero de observaciones. No hay la suficiente informacion para
+# entrenar un modelo. Ademas en este cas buscamos hacer una interpolacion
+# para predecir el sueldo de un empleado entre estos niveles.
+# Sin embargo este paso es escencial.
+
 """
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 """
 
-# Escalado de variables
+################################################
+#            Escalado de variables             #
+################################################
+
+# No se hara. El modelo de RLP busca entender las relaciones no lineales.
+# Si se escalan los datos, esta relacion NO LINEAL podria perderse.
 """from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
 X_test = sc_X.transform(X_test)"""
 
-# Ajustar la regresión lineal con el dataset
+##################################################
+#      REFUTACION LA Ho (REGRESION LINEAL)       #
+##################################################
+
+# Generamos una regresion lineal para ver lo que pasa cuando se intenta 
+# ajustar datos que no son lineales. veremos despues como regresa la cosa
+# con una regresion lineal polinomica.
+
+## Ajustar la regresión lineal con todo el dataset
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 
-# Ajustar la regresión polinómica con el dataset
+############################################################
+##  Ajustar la regresión polinómica con todo el dataset    #
+############################################################
+
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 4)
+# Seleccionamos el grado hasta el que queremos nuestra regresion pilinomica
+# Por defecto es de grado 2.
+poly_reg = PolynomialFeatures(degree=6)
+# Transformamos nuestra matriz de caracteristicas, para generar tantas 
+# nuevas columnas como grados del polinomio que hayamos elegido.
+# Estas nuevas columnas tendran los valores de la columna original pero 
+# elevados a tantas potencias, como grados tenga el polinomio.
+
+# Transformamos nuestra matriz. La funcion añade una colunmna de 1's hasta
+# el principio que servira para la obtencion del termino independiente 
+# en la ecuacion polinomial.
 X_poly = poly_reg.fit_transform(X)
+# Ahora hacemos el ajuste polinomial y creamos nustros objeto que hace
+# nuestra regresion lineal. Es la misma funcion que para la multiple.
 lin_reg_2 = LinearRegression()
+# Ajustamos nuestra regresion a los datos.
 lin_reg_2.fit(X_poly, y)
 
-# Visualización de los resultados del Modelo Lineal
-plt.scatter(X, y, color = "red")
-plt.plot(X, lin_reg.predict(X), color = "blue")
+################################################
+#        VISUALIZACION DE RESULTADOS           #
+################################################
+
+# Visualización de los resultados del MODELO LINEAL
+plt.scatter(X, y, color="red")
+plt.plot(X, lin_reg.predict(X), color="blue")
 plt.title("Modelo de Regresión Lineal")
 plt.xlabel("Posición del empleado")
 plt.ylabel("Sueldo (en $)")
 plt.show()
 
-# Visualización de los resultados del Modelo Polinómico
+# Pdemos ver que existe un mal ajuste de los datos.
+
+## Visualización de los resultados del MODELO POLINÓMICO
+
+# Para evitar que la funcion continua que predice el salario ,se vea 
+# como trocitos de recta, se creea una secuencia de valores que esten
+# en ptos. intermedios entre el 1y el 10, que vayan de 0.1 en 0.1.
 X_grid = np.arange(min(X), max(X), 0.1)
+# Convertimos el vector a una matriz de 1 columna.
 X_grid = X_grid.reshape(len(X_grid), 1)
-plt.scatter(X, y, color = "red")
-plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = "blue")
+plt.scatter(X, y, color="red")
+# Con esta parte hacemos una transfoemcion de la matriz de la X_grid
+# que creeamos previamente y hacemos la prediccion de los valores.   
+plt.plot(X_grid, lin_reg_2.predict(
+    poly_reg.fit_transform(X_grid)), color="blue")
 plt.title("Modelo de Regresión Polinómica")
 plt.xlabel("Posición del empleado")
 plt.ylabel("Sueldo (en $)")
 plt.show()
 
+# NOTA: Hay que ir subiendo de grado el polinomio para ir generando cada
+# vez un mejor ajuste hasta quedarnos con un modelo deseable.
+# Cuidado con poner grados de mas.
+
+################################################
+#                PREDICCION                    # 
+################################################
+
+# Finalmente averiguaremos el sueldo que se espera de un empleado de nivel 
 # Predicción de nuestros modelos
-lin_reg.predict(6.5)
-lin_reg_2.predict(poly_reg.fit_transform(6.5))
-
-
-
-
-
-
+# PARA EL MODELO DE RLM
+lin_reg.predict([[6.5]]) # Creamos un nd adarray
+# PARA EL MODELO DE RP
+lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
